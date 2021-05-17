ed656fb6886bf7c51312c918c1ca3c7015555f95
vvakhlyn <vvakhlyn@mts.ru>
2021-05-18 00:57:37 +0300

    first working version

---
+++ cmd/ocp-chat-api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/rs/zerolog/log"
+
+var defaultLogger = log.Logger.With().Timestamp().Logger()
+
+func main() {
+	if err := Run(); err != nil {
+		defaultLogger.Fatal().Err(err).Msg("run application")
+	}
+}
+
+func Run() error {
+	defaultLogger.Info().Msg("Hi, Victor Akhlynin will write this project")
+	return nil
+}
