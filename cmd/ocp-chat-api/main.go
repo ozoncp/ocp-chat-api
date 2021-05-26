b5b1c4b7179582b74eb30c813f48eb311cb744f7
letsdoitnow <ahlininv@gmail.com>
2021-05-26 23:53:51 +0300

    file open

--- cmd/ocp-chat-api/main.go
+++ cmd/ocp-chat-api/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/rs/zerolog/log"
+import (
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"os"
+)
 
 var defaultLogger = log.Logger.With().Timestamp().Logger()
 
@@ -12,5 +16,18 @@ func main() {
 
 func Run() error {
 	defaultLogger.Info().Msg("Hi, Victor Akhlynin will write this project")
+
+	for i := 0; i < 100; i++ {
+		f, err := os.Open("go.mod")
+		if err != nil {
+			return errors.Wrap(err, "open file")
+		}
+		defaultLogger.Info().Msg("open successful")
+		defer func() {
+			if err := f.Close(); err != nil {
+				defaultLogger.Error().Err(err).Msg("close file bad")
+			}
+		}()
+	}
 	return nil
 }
