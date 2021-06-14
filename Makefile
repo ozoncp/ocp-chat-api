e8139e23482df9fbd9aa5af7620c7426f3921b07
letsdoitnow <ahlininv@gmail.com>
2021-06-14 13:31:26 +0300

    refactor protofile and all dependent on it, add puml

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ run: build
 	@./bin/ocp-chat-api
 
 test:
-	go test -test.v ./...
+	go test -test.v -coverprofile=coverage.out ./...
 
 cover: test
 	go tool cover -html=coverage.out
@@ -18,3 +18,5 @@ generate-mocks:
 
 grpc-proto:
 	protoc --proto_path=pkg/chat_api --go_out=pkg/chat_api  --go_opt=paths=source_relative --go-grpc_out=pkg/chat_api --go_opt=paths=source_relative ocp-chat-api.proto
+
+all: test lint generate-mocks grpc-proto
--- pkg/chat_api/chat_api.go
+++ pkg/chat_api/chat_api.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Service interface {
-	CreateChat(ctx context.Context, id, classroom uint64, link string) error
+	CreateChat(ctx context.Context, classroom uint64, link string) error
 	DescribeChat(ctx context.Context, id uint64) (string, error)
 	RemoveChat(ctx context.Context, id uint64) error
 	ListChats(ctx context.Context) ([]string, error)
@@ -24,51 +24,50 @@ func New(service Service) *ChatAPI {
 }
 
 func (s *ChatAPI) CreateChat(ctx context.Context, req *CreateChatRequest) (*CreateChatResponse, error) {
-	if err := s.service.CreateChat(ctx, req.Id, req.ClassroomId, req.Link); err != nil {
+	if err := s.service.CreateChat(ctx, req.ClassroomId, req.Link); err != nil {
 		return nil, errors.Wrap(err, "create chat")
 	}
 
 	return &CreateChatResponse{
-		Code:    200,
+		Id:      2423423423,
 		Message: "ok",
 	}, nil
 }
 
 func (s *ChatAPI) DescribeChat(ctx context.Context, req *DescribeChatRequest) (*DescribeChatResponse, error) {
 	return &DescribeChatResponse{
-		Id:          222,
-		ClassroomId: 111,
-		Link:        "asdfasfsadf.com",
+		Chat: &ChatInstance{
+			Id:          222,
+			ClassroomId: 111,
+			Link:        "asdfasfsadf.com",
+		},
 	}, nil
 }
 
 func (s *ChatAPI) RemoveChat(ctx context.Context, req *RemoveChatRequest) (*RemoveChatResponse, error) {
-	return &RemoveChatResponse{
-		Code:    200,
-		Message: "delete ok",
-	}, nil
+	return &RemoveChatResponse{}, nil
 }
 
 func (s *ChatAPI) ListChats(ctx context.Context, req *ListChatsRequest) (*ListChatsResponse, error) {
-	c1 := &ListChatOne{
-		Id:          11,
+	c1 := &ChatInstance{
+		Id:          222,
 		ClassroomId: 111,
 		Link:        "asdfasfsadf.com",
 	}
 
-	c2 := &ListChatOne{
-		Id:          22,
-		ClassroomId: 222,
-		Link:        "asdfasfsadf2.com",
+	c2 := &ChatInstance{
+		Id:          222,
+		ClassroomId: 111,
+		Link:        "asdfasfsadf.com",
 	}
 
-	c3 := &ListChatOne{
-		Id:          33,
-		ClassroomId: 333,
-		Link:        "asdfasfsadf3.com",
+	c3 := &ChatInstance{
+		Id:          222,
+		ClassroomId: 111,
+		Link:        "asdfasfsadf.com",
 	}
 
-	chats := []*ListChatOne{c1, c2, c3}
+	chats := []*ChatInstance{c1, c2, c3}
 	return &ListChatsResponse{
 		Packet: chats,
 	}, nil
